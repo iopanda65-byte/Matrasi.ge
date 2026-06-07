ecf1806bc14c8651a8983f63fdcebbc4216b3eaa
Your Name <you@example.com>
2026-06-07 22:48:22 +0400

    fix navbar import and add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Navbar from "./Components/Navbar";
+import Navbar from "./Components/NavBar";
 
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
