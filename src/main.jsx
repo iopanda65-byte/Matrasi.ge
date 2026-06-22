656c096bd28842ad1ef51021f84433d51fa27b9d
Davit <iopanda65@gmail.com>
2026-06-22 16:00:32 +0400

    added google analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { HelmetProvider } from 'react-helmet-async'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { HelmetProvider } from "react-helmet-async";
+import ReactGA from "react-ga4"; // 1. შემოვიტანეთ ანალიტიკის ბიბლიოთეკა
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// 2. გავაკეთეთ ინიციალიზაცია შენი Measurement ID-ით
+ReactGA.initialize("G-T7R5RDYZ5L");
+
+// 3. დავაფიქსირეთ საიტზე შემოსვლის (პირველი გვერდის) ნახვა
+ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HelmetProvider>
       <App />
     </HelmetProvider>
   </React.StrictMode>,
-)
+);
